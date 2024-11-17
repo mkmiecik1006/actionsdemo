ce75fa205ddbfc19946dcd76b97e93a473561a4a
kmieta <mkmiecik626@gmail.com>
2024-11-17 09:49:31 +0100

    allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   projectId: "kac7ue",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-
+      allureCypress(on, config, {
+              resultsDir: "allure-results",
+            });
+            return config;
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,9 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import "allure-cypress";
 import './commands'
+
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
